7a5bbc6ebabeb751f437cdc8c9ff95c670f2cdf5
neojarvis <techadmin@iamneo.ai>
2025-04-10 11:39:31 +0000

    Manual-save-commit-10/04/2025--17:09:30

--- sample.sh
+++ sample.sh
@@ -1,8 +1,8 @@
 git init
 git remote -v
 git remote remove origin
-git remote add origin "https://github.com/Divakar308/sampletest.git"
+git remote add origin "https://github.com/Divakar308/demmmm.git"
 git remote -v
-git add .
+git add ./dotnetapp
 git commit -m "initial commit"
 git push -u origin main
