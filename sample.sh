c0001c0be4d27496021188a675fb876ada95e17e
neojarvis <techadmin@iamneo.ai>
2025-04-10 11:30:17 +0000

    initial commit

--- sample.sh
+++ sample.sh
@@ -1,6 +1,6 @@
 git init
 git remote -v
-git remote add origin "https://github.com/Divakar308/demotest.git"
+git remote add origin "https://github.com/Divakar308/sampletest.git"
 git remote -v
 git add .
 git commit -m "initial commit"
